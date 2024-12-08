d2b28dff198127294f607ce88f8d643ce7aa0fd0
“NhiLuu” <nhi.luu@team.enouvo.com>
2024-12-08 17:47:05 +0700

    Initial Commit

---
+++ SwiftFirebaseLearn/SwiftFirebaseLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseLearnApp.swift
+//  SwiftFirebaseLearn
+//
+//  Created by Luu Yen Nhi on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
