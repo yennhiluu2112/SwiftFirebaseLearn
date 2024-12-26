5d122822d42c488219c1c530b97bc88467324743
“NhiLuu” <nhi.luu@team.enouvo.com>
2024-12-26 23:15:37 +0700

    sort, filter, query collection with firestore

--- SwiftFirebaseLearn/Core/Product/ProductsView.swift
+++ SwiftFirebaseLearn/Core/Product/ProductsView.swift
@@ -10,12 +10,62 @@ import SwiftUI
 @MainActor
 final class ProductsViewModel: ObservableObject {
     @Published private(set) var products: [Product] = []
+    @Published var selectedFilter: FilterOption? = nil
+    @Published var selectedCategory: CategoryOption? = nil
+
+//    func getAllProducts() async throws {
+//        self.products = try await ProductsManager.shared.getAllProducts()
+//    }
     
-    func getAllProducts() async throws {
-        self.products = try await ProductsManager.shared.getAllProducts()
+    enum FilterOption: String, CaseIterable {
+        case none
+        case priceHigh
+        case priceLow
+        
+        var priceDecending: Bool? {
+            switch self {
+            case .none:
+                return nil
+            case .priceLow:
+                return false
+            case .priceHigh:
+                return true
+            }
+        }
     }
     
+    func filterSelected(option: FilterOption) {
+        self.selectedFilter = option
+        getProducts()
+    }
+    
+    enum CategoryOption: String, CaseIterable {
+        case smartphone
+        case laptop
+        case fragrances
+        case none
+        
+        var categoryKey: String? {
+            if self == .none {
+                return nil
+            }
+            return self.rawValue
+        }
+    }
+    
+    func categorySelected(option: CategoryOption) {
+        self.selectedCategory = option
+        getProducts()
+    }
+    
+    func getProducts() {
+        Task {
+            self.products = try await ProductsManager.shared.getAllProductsByPrice(priceDescending: selectedFilter?.priceDecending, forCategory: selectedCategory?.categoryKey)
+        }
+    }
 }
+
+ 
 
 struct ProductsView: View {
     
@@ -27,12 +77,35 @@ struct ProductsView: View {
             }
         }
         .navigationTitle("Products")
-        .task {
-            try? await viewModel.getAllProducts()
-        }
-//        .onAppear {
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Menu("Filter: \(viewModel.selectedFilter?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.FilterOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            viewModel.filterSelected(option: option)
+                            
+                        }
+                    }
+                    
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu("Category: \(viewModel.selectedCategory?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            viewModel.categorySelected(option: option)
+                            
+                        }
+                    }
+                    
+                }
+            }
+        })
+        .onAppear {
 //            viewModel.downloadProductsAndUploadToFirebase()
-//        }
+            viewModel.getProducts()
+        }
     }
 }
 
--- SwiftFirebaseLearn/Firestore/ProductsManager.swift
+++ SwiftFirebaseLearn/Firestore/ProductsManager.swift
@@ -29,6 +29,36 @@ final class ProductsManager {
     func getProduct(id: String) async throws -> Product {
         try await productDocument(id: id).getDocument(as: Product.self)
     }
+    
+    private func getAllProductsSortedByPrice(descending: Bool) async throws -> [Product]  {
+        try await productCollection
+            .order(by: Product.CodingKeys.price.rawValue, descending: descending)
+            .getAllDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsForCategory(category: String) async throws -> [Product]  {
+        try await productCollection
+            .whereField(Product.CodingKeys.category.rawValue, isEqualTo: category)
+            .getAllDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsByPrice(descending: Bool, category: String) async throws -> [Product] {
+        try await productCollection
+            .whereField(Product.CodingKeys.category.rawValue, isEqualTo: category)
+            .order(by: Product.CodingKeys.price.rawValue, descending: descending)
+            .getAllDocuments(as: Product.self)
+    }
+    
+    func getAllProductsByPrice(priceDescending descending: Bool?, forCategory category: String?) async throws -> [Product] {
+        if let descending, let category {
+            return try await getAllProductsByPrice(descending: descending, category: category)
+        } else if let descending {
+            return try await getAllProductsSortedByPrice(descending: descending)
+        } else if let category {
+            return try await getAllProductsForCategory(category: category)
+        }
+        return try await getAllProducts()
+    }
 }
 
 extension Query {
